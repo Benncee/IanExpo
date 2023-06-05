85bc28b8c07cb74dd3387d98190b274534812bf1
Markymy <rocabertemarky09@gmail.com>
2023-06-05 19:15:35 +0800

    expoApp

--- App.js
+++ App.js
@@ -1,10 +1,49 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleRegistration = () => {
+    // Perform registration logic here
+    // You can use the captured name, email, and password
+    console.log('Name:', name);
+    console.log('Email:', email);
+    console.log('Password:', password);
+
+    // Clear the form fields
+    setName('');
+    setEmail('');
+    setPassword('');
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Registration Form</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Name"
+        value={name}
+        onChangeText={setName}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+        keyboardType="email-address"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      <Button title="Register" onPress={handleRegistration} />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +55,13 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    width: '80%',
+    height: 40,
+    borderWidth: 1,
+    borderColor: 'gray',
+    marginBottom: 10,
+    paddingLeft: 10,
   },
 });
